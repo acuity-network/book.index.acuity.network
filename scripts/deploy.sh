fb6c852a6976ac50be587d1cdb0d54444134d9bc
Jonathan Brown <jbrown@acuity.social>
2024-05-13 08:53:34 +0700

    Fix deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mdbook build
-rsync -avhP --stats --del book/ book.hybridscan.app:hybrid-book
+rsync -avhP --stats --del book/ book.index.acuity.network:book.index.acuity.network
